948bcaaabf7e69734fe28a0006b814617e535b0e
Rias <hey@rias.be>
2023-10-16 12:31:38 +0200

    Return cached version on error

--- src/ContentApi.php
+++ src/ContentApi.php
@@ -2,7 +2,6 @@
 
 namespace Spatie\ContentApi;
 
-use Illuminate\Cache\Repository;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
@@ -12,64 +11,59 @@ class ContentApi
 {
     const BASE_URL = 'https://content.spatie.be/api';
 
-    private static function getCache(): Repository
-    {
-        $cache = Cache::store();
-
-        if ($cache->supportsTags()) {
-            $cache = $cache->tags(['content_api']);
-        }
-
-        return $cache;
-    }
-
     public static function getPosts(string $product, int $page = 1, int $perPage = 20, string $sort = '-date'): Paginator
     {
-        return static::getCache()
-            ->remember(
-                key: "posts-{$product}-{$page}-{$perPage}",
-                ttl: now()->addHour(),
-                callback: function () use ($sort, $page, $perPage, $product) {
-                    $response = Http::get(static::BASE_URL.'/collections/posts/entries', [
-                        'filter' => [
-                            'product' => $product,
-                        ],
-                        'limit' => $perPage,
-                        'page' => $page,
-                        'sort' => $sort,
-                    ])->json();
+        $response = Http::get(static::BASE_URL.'/collections/posts/entries', [
+            'filter' => [
+                'product' => $product,
+            ],
+            'limit' => $perPage,
+            'page' => $page,
+            'sort' => $sort,
+        ]);
 
-                    return new Paginator(
-                        items: collect($response['data'])->map(function (array $post) {
-                            return Post::fromResponse($post);
-                        }),
-                        perPage: $perPage,
-                        currentPage: $page,
-                    );
-                }
-            );
+        if (! $response->successful()) {
+            report($response->toException());
+
+            if ($posts = Cache::get("posts-{$product}-{$page}-{$perPage}-{$sort}")) {
+                return $posts;
+            }
+
+            return new Paginator([], $perPage, $page);
+        }
+
+        $posts = new Paginator(
+            items: collect($response['data'])->map(function (array $post) {
+                return Post::fromResponse($post);
+            }),
+            perPage: $perPage,
+            currentPage: $page,
+        );
+
+        Cache::put("posts-{$product}-{$page}-{$perPage}-{$sort}", $posts);
+
+        return $posts;
     }
 
     public static function getPost(string $product, string $slug): ?Post
     {
-        return static::getCache()
-            ->remember(
-                key: "post-{$product}-{$slug}",
-                ttl: now()->addHour(),
-                callback: function () use ($slug, $product) {
-                    $post = Http::get(self::BASE_URL.'/collections/posts/entries', [
-                        'filter' => [
-                            'product' => $product,
-                            'slug' => $slug,
-                        ],
-                    ])->json('data.0');
+        $response = Http::get(self::BASE_URL.'/collections/posts/entries', [
+            'filter' => [
+                'product' => $product,
+                'slug' => $slug,
+            ],
+        ]);
 
-                    if (! $post) {
-                        return null;
-                    }
+        if ($response->serverError()) {
+            report($response->toException());
 
-                    return Post::fromResponse($post);
-                }
-            );
+            return Cache::get("post-{$product}-{$slug}");
+        }
+
+        $post = Post::fromResponse($response->json('data.0'));
+
+        Cache::put("post-{$product}-{$slug}", $post);
+
+        return $post;
     }
 }
---
+++ tests/ContentApiTest.php
@@ -0,0 +1,77 @@
+<?php
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+use Spatie\ContentApi\ContentApi;
+
+beforeEach(function () {
+    Http::preventStrayRequests();
+
+});
+
+it('can get posts', function () {
+    Http::fake([
+        'https://content.spatie.be/api/collections/posts/entries?filter%5Bproduct%5D=mailcoach&limit=20&page=1&sort=-date' => Http::response([
+            'data' => [
+                ['title' => 'A post', 'slug' => 'a-post', 'header_image' => null, 'summary' => 'summary', 'authors' => [], 'content' => [], 'published' => true, 'date' => now()->format('Y-m-d H:i:s'), 'updated_at' => now()->format('Y-m-d H:i:s')],
+            ],
+        ])
+    ]);
+
+    $posts = ContentApi::getPosts('mailcoach');
+
+    expect($posts->count())->toBe(1);
+    expect(Cache::has("posts-mailcoach-1-20--date"))->toBeTrue();
+});
+
+it('returns a cached version when the request fails', function () {
+    Http::fake([
+        'https://content.spatie.be/api/collections/posts/entries?filter%5Bproduct%5D=mailcoach&limit=20&page=1&sort=-date' => Http::sequence([
+            Http::response([
+                'data' => [
+                    ['title' => 'A post', 'slug' => 'a-post', 'header_image' => null, 'summary' => 'summary', 'authors' => [], 'content' => [], 'published' => true, 'date' => now()->format('Y-m-d H:i:s'), 'updated_at' => now()->format('Y-m-d H:i:s')],
+                ],
+            ]),
+            Http::response([], 500),
+        ]),
+    ]);
+
+    ContentApi::getPosts('mailcoach');
+    $posts = ContentApi::getPosts('mailcoach');
+
+    expect($posts->count())->toBe(1);
+});
+
+it('can get a post', function () {
+    Http::fake([
+        'https://content.spatie.be/api/collections/posts/entries?filter%5Bproduct%5D=mailcoach&filter%5Bslug%5D=a-post' => Http::response([
+            'data' => [
+                ['title' => 'A post', 'slug' => 'a-post', 'header_image' => null, 'summary' => 'summary', 'authors' => [], 'content' => [], 'published' => true, 'date' => now()->format('Y-m-d H:i:s'), 'updated_at' => now()->format('Y-m-d H:i:s')],
+            ],
+        ])
+    ]);
+
+    $post = ContentApi::getPost('mailcoach', 'a-post');
+
+    expect($post->title)->toBe("A post");
+    expect(Cache::has("post-mailcoach-a-post"))->toBeTrue();
+});
+
+it('returns a cached version of a post when the request fails', function () {
+    Http::fake([
+        'https://content.spatie.be/api/collections/posts/entries?filter%5Bproduct%5D=mailcoach&filter%5Bslug%5D=a-post' => Http::sequence([
+            Http::response([
+                'data' => [
+                    ['title' => 'A post', 'slug' => 'a-post', 'header_image' => null, 'summary' => 'summary', 'authors' => [], 'content' => [], 'published' => true, 'date' => now()->format('Y-m-d H:i:s'), 'updated_at' => now()->format('Y-m-d H:i:s')],
+                ],
+            ]),
+            Http::response([], 500),
+        ]),
+    ]);
+
+    ContentApi::getPost('mailcoach', 'a-post');
+    $post = ContentApi::getPost('mailcoach', 'a-post');
+
+    expect($post->title)->toBe("A post");
+    expect(Cache::has("post-mailcoach-a-post"))->toBeTrue();
+});
