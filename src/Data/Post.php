fed853989c62758141c25234183928d172bcae32
Rias <hey@rias.be>
2023-10-16 14:04:23 +0200

    Add support for tags

--- src/Data/Post.php
+++ src/Data/Post.php
@@ -12,10 +12,13 @@ final readonly class Post
         public string $title,
         public string $slug,
         public ?string $header_image,
-        public string $summary,
+        public ?string $summary,
 
         /** @property Collection<Author> */
         public Collection $authors,
+
+        /** @property string[] */
+        public array $tags,
 
         /** @property Collection<Set> */
         public Collection $content,
@@ -35,6 +38,7 @@ final readonly class Post
             summary: $post['summary'],
             authors: collect($post['authors'])
                 ->map(fn (array $author) => Author::fromResponse($author)),
+            tags: $post['tags'] ?? [],
             content: collect($post['content'])
                 ->map(fn (array $content) => Set::fromResponse($content)),
             published: $post['published'],
