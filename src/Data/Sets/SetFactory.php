5faa9d9f8ed46f194757324d334ae4b6f075d305
Rias <hey@rias.be>
2023-12-14 13:44:49 +0100

    Use value

--- src/Data/Sets/SetFactory.php
+++ src/Data/Sets/SetFactory.php
@@ -9,9 +9,9 @@ class SetFactory
     public static function fromResponse(array $response): Set
     {
         return match ($response['type']) {
-            SetType::Text => new Text($response['text']),
-            SetType::Code => new Code($response['code'], $response['caption']),
-            SetType::Video => new Video($response['video'], $response['embed_url'], $response['video_id']),
+            SetType::Text->value => new Text($response['text']),
+            SetType::Code->value => new Code($response['code'], $response['caption']),
+            SetType::Video->value => new Video($response['video'], $response['embed_url'], $response['video_id']),
         };
     }
 }
