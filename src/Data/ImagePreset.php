d59e6c187645e0bdbf7197cd81994213fe837de4
riasvdv <3626559+riasvdv@users.noreply.github.com>
2025-02-11 16:03:29 +0000

    Fix styling

--- src/Data/ImagePreset.php
+++ src/Data/ImagePreset.php
@@ -4,14 +4,12 @@ namespace Spatie\ContentApi\Data;
 
 class ImagePreset
 {
-
     public function __construct(
         public string $name,
         public string $url,
         public int $width,
         public int $height,
-    ) {
-    }
+    ) {}
 
     public static function fromResponse(string $name, array $imagePreset): self
     {
